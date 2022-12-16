c72eb92a51100e7a34e2c905a587d612201633bf
Gwhoami <gwhoamihelp@gmail.com>
2022-12-15 22:57:28 -0500

    removed folder

--- gwhoami/src/util/maincontext.js
+++
@@ -1,9 +0,0 @@
-import{ createContext } from 'react';
-
-export const MainContext = createContext({});
-
-export const MenuContext = createContext({});
-
-export const HomeContext = createContext({});
-
-export const UserContext = createContext({});
